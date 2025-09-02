68315bc6db27bfb7c5d1118e29b41955a1d51d37
laiba-786 <laibawaheed145@gmail.com>
2025-09-02 15:40:32 +0500

    My first commit

---
+++ tests/fileUpload.spec.js
@@ -0,0 +1,25 @@
+import { test, expect, chromium } from '@playwright/test';
+
+test('launch browser with context', async () => {
+  // Launch browser
+  const browser = await chromium.launch({ headless: false }); // set true for CI
+
+  // Create a new isolated context
+  const context = await browser.newContext({
+    viewport: { width: 1280, height: 800 },
+    // storageState: 'storage-state.json', // optionally reuse auth/session
+    // userAgent: 'MyCustomAgent/1.0',
+  });
+
+  // Open a page in this context
+  const page = await context.newPage();
+  await page.goto('https://demo.automationtesting.in/FileDownload.html');
+  page.getByRole('link', { name: 'Download', exact: true }).click();
+  const downloadPromise = page.waitForEvent('download');
+  const dowonload = await downloadPromise;
+
+  await page.pause();
+  // Cleanup
+  await context.close();
+  await browser.close();
+});
